0152a5464d1272a4ca597c64c9a17b5ce7e584dc
Piyush Garg <108219267+piyushgargdev-01@users.noreply.github.com>
2023-03-11 10:22:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const userRoute = require("./routes/user");
 const app = express();
 const PORT = 8001;
 
-connectToMongoDB("mongodb://localhost:27017/short-url").then(() =>
+connectToMongoDB(process.env.MONGODB ?? "mongodb://localhost:27017/short-url").then(() =>
   console.log("Mongodb connected")
 );
 
